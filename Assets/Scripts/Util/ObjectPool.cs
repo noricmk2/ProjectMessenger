e057c39e1f57f0515396187e75cbeba11da4421a
noricmk2 <noric123@gmail.com>
2019-03-13 22:36:27 +0900

    기초작업 진행중2/

---
+++ Assets/Scripts/Util/ObjectPool.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ObjectPool<T>
+{
+    public delegate T CreateAction();
+
+    private readonly CreateAction m_CreateAction;
+    private readonly Action<T> m_PushAction;
+    private readonly Action<T> m_PopAction;
+
+    private Stack<T> m_Pool;
+    private List<T> m_RestoreList;
+
+    public int Count
+    {
+        get { return m_Pool.Count; }
+    }
+
+    public ObjectPool(int count, CreateAction createAction, Action<T> pushAction, Action<T> popAction)
+    {
+        m_CreateAction = createAction;
+        m_PushAction = pushAction;
+        m_PopAction = popAction;
+
+        for (int i = 0; i < count; ++i)
+            Add();
+    }
+
+    public void Push(T pushObj)
+    {
+        if (m_Pool.Contains(pushObj))
+            return;
+        m_Pool.Push(pushObj);
+
+        if (m_RestoreList.Contains(pushObj))
+            m_RestoreList.Remove(pushObj);
+
+        if (m_PushAction != null)
+            m_PushAction(pushObj);
+    }
+
+    public T Pop()
+    {
+        if (m_Pool.Count <= 0)
+            Add();
+
+        var retObj = m_Pool.Pop();
+        if (m_PopAction != null)
+            m_PopAction(retObj);
+        m_RestoreList.Add(retObj);
+        return retObj;
+    }
+
+    private void Add()
+    {
+        if (m_CreateAction != null)
+        {
+            var poolObj = m_CreateAction();
+            Push(poolObj);
+        }
+    }
+
+    public void Restore()
+    {
+        for (int i = 0; i < m_RestoreList.Count; ++i)
+            Push(m_RestoreList[i]);
+    }
+}
---
+++ Assets/Scripts/Util/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSUtil
+{
+    public static class Func
+    {
+        #region Extension Method
+        public static void SetActive_Check(this GameObject gameObj, bool bActive)
+        {
+            if (gameObj.activeSelf != bActive)
+            {
+                gameObj.SetActive(bActive);
+            }
+        }
+        #endregion
+    }
+
+    public class ConstValue        //전역적으로 쓰이는 고정값
+    {
+        public static readonly Vector2 DEFULT_SCREEN_SIZE = new Vector2(1280f, 720f);
+    }
+
+    public class ColorPalette        //컬러 상수값
+    {
+
+    }
+}
