aa9e717795c1a46b67ef9d60c1563927a1945d28
noricmk2 <noric123@gmail.com>
2019-07-27 20:47:33 +0900

    리사이클 스크롤(에디터 스크립트)

---
+++ Assets/Editor/RecycleScrollEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(RecycleScroll))]
+public class RecycleScrollEditor : Editor
+{
+    private RecycleScroll m_Scroll;
+
+    private void OnEnable()
+    {
+        m_Scroll = target as RecycleScroll;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        m_Scroll.content = EditorGUILayout.ObjectField("Content", m_Scroll.content, typeof(RectTransform), true) as RectTransform;
+        m_Scroll.viewport = EditorGUILayout.ObjectField("Viewport", m_Scroll.viewport, typeof(RectTransform), true) as RectTransform;
+        m_Scroll.horizontal = EditorGUILayout.Toggle("Horizontal", m_Scroll.horizontal);
+        m_Scroll.vertical = EditorGUILayout.Toggle("Vertical", m_Scroll.vertical);
+        m_Scroll.movementType = (ScrollRect.MovementType)EditorGUILayout.EnumPopup("MovementType", m_Scroll.movementType);
+        m_Scroll.LayoutGroup = EditorGUILayout.ObjectField("LayoutGroup", m_Scroll.LayoutGroup, typeof(LayoutGroup), true) as LayoutGroup;
+    }
+}
